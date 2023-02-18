66853deeaca13b2aec4de80fff68335c6051ff2d
Naod Hunde <chris_naod@yahoo.com>
2023-02-19 02:01:25 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const Sequelize = require('sequelize');
+
+// create a new Sequelize instance
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
+module.exports = sequelize;
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
